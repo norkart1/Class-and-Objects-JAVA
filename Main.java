8b38a45faf1b77e92d3873ae9957161e0434f3b3
norkart1 <138743746+norkart1@users.noreply.github.com>
2023-12-06 12:15:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    Sample s1=new Sample();
+   Sample s2=new Sample();
+
+   s1.a=10;
+   s2.a=20;
+   s1.b=30;
+   s2.b=40;
+
+   s1.display();
+   s2.display();
+  }
+}
---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+ int a;
+ int b;
+
+ void display() {
+  System.out.println("a = " + a  "  b = " + b);
+
+ }
+}
